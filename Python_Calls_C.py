587cbbce0d209f3cd4ea60dc8ed9ab6971e0ba1b
Jigar Shah <jshah@pinterest.com>
2022-06-23 15:33:34 -0700

    Extra

--- Python_Calls_C.py
+++
@@ -1,6 +0,0 @@
-from distutils.core import setup, Extension
-
-setup(name = 'CExtDemo',
-      version = '1.0',
-      description = 'Show how to make basic C extensions',
-      ext_modules = [Extension('demo', sources = ['demo.c'])])
